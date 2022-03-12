4101e027f413a139c420763de98fad7ce0fbe67a
AlbertTestGit <albert.git1@gmail.com>
2022-03-12 15:41:32 +0500

    097 Using AutoMapper queryable extensions

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using AutoMapper;
 
 namespace API.Helpers;
@@ -12,7 +13,11 @@ public class AutoMapperProfiles : Profile
             .ForMember(
                 dest => dest.PhotoUrl,
                 opt => opt.MapFrom(src =>
-                    src.Photos.FirstOrDefault(x => x.IsMain).Url));
+                    src.Photos.FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(
+                dest => dest.Age,
+                opt => opt.MapFrom(src =>
+                    src.DateOfBirth.CalculateAge()));
         CreateMap<Photo, PhotoDto>();
     }
 }
